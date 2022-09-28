873db37a0db838011d039268b2512cd6050420e3
DehqonovFazliddin <fazliddindehqonov092@gmail.com>
2022-09-28 17:24:36 +0500

    working on showing emails

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -22,27 +22,75 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+function fetchEmail(email, color = "white") {
+  const email_view = document.getElementById("emails-view")
+  const card = document.createElement("div")
+  const card_body = document.createElement('div')
+  const param = document.createElement("p")
+  const link = document.createElement('a')
+  card.classList.add("card")
+  card.classList.add("mt-2")
+  card_body.classList.add("card-body")
+  param.innerHTML += "<b>" + email.sender + "</b>" + "<span class='tab'></span>";
+  param.innerHTML += (email.body.substring(0, 15));
+  param.innerHTML += "<span class='float-right text-muted'>" + email.timestamp + "</span>"
+  link.setAttribute("id", "show-email")
+  link.setAttribute("data-id", email.id)
+  link.setAttribute("onclick", `showEmail(${email.id})`)
+  link.appendChild(param)
+  card_body.appendChild(link)
+  card.appendChild(card_body)
+  email_view.appendChild(card)
+  if (color !== "white") {
+    card.classList.add("read")
+  }
+}
+function test() {
+  console.log("clicked");
+}
+function showEmail(email_id) {
+  fetch(`/emails/${email_id}`)
+    .then(response => response.json())
+    .then(email => {
+      console.log(`email id: ${email.id}`);
+    })
+
+}
+
 function load_mailbox(mailbox) {
   if (mailbox === "inbox") {
     fetch('/emails/inbox')
-    .then(response => response.json())
-    .then(emails => {
-      for (let index = 0; index < emails.length; index++) {
-        const email_view = document.getElementById("emails-view")
-        const card = document.createElement("div")
-        card.classList.add("card")
-        card.classList.add("mt-2")
-        const card_body = document.createElement('div')
-        card_body.classList.add("card-body")
-        const param = document.createElement("p")
-        param.innerHTML += "<b>" + emails[index].sender + "</b>" + "<span class='tab'></span>";
-        param.innerHTML += (emails[index].body.substring(0, 15));
-        param.innerHTML += "<span class='float-right'>" + emails[index].timestamp + "</span>"
-        card_body.appendChild(param)
-        card.appendChild(card_body)
-        email_view.appendChild(card)
-      }
-    });
+      .then(response => response.json())
+      .then(emails => {
+        for (let index = 0; index < emails.length; index++) {
+          if (emails[index].read) {
+            fetchEmail(emails[index], color="gray")
+          } else {
+            fetchEmail(emails[index])            
+          }
+        }
+      });
+  }
+  else if (mailbox === "archive") {
+    fetch('/emails/archive')
+      .then(response => response.json())
+      .then(emails => {
+        for (let index = 0; index < emails.length; index++) {
+          fetchEmail(emails[index], color="gray")
+        }
+      });
+  }
+  else if (mailbox === 'sent') {
+    fetch('/emails/sent')
+      .then(response => response.json())
+      .then(emails => {
+        for (let index = 0; index < emails.length; index++) {
+          fetchEmail(emails[index])
+        }
+      });
+  }
+  else if (document.getElementById("show-email").clicked == true) {
+    console.log("hello world");
   }
 
   // Show the mailbox and hide other views
